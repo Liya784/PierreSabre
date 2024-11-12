ceef76be4dd0e6571bff44106cee1e541bd4402e
Liya Paret <prm5166a@ensfsi.onmicrosoft.com>
2024-11-12 09:17:04 +0100

    connaissances des humains

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -1,20 +1,23 @@
 package personnages;
 
+import java.util.ArrayList;
+
 public class Humain {
 	private String nom;
 	private String boisson;
 	protected int argent;
+	private ArrayList<Humain> memoire;
+	public static final int NB_CONNAISSANCES_MAX = 3;
 
 	public Humain(String nom, String boisson, int argent) {
 		this.nom = nom;
 		this.boisson = boisson;
 		this.argent = argent;
-
+		this.memoire = new ArrayList<>();
 	}
 
 	public void parler(String texte) {
 		System.out.println("(" + getNom() + ")" + " - " + texte);
-
 	}
 
 	public String getNom() {
@@ -31,7 +34,6 @@ public class Humain {
 
 	public void boire() {
 		parler("Mmmm, un bon verre de " + boisson + " ! GLOUPS !");
-
 	}
 
 	public void acheter(String bien, int prix) {
@@ -50,7 +52,34 @@ public class Humain {
 
 	protected void perdreArgent(int perte) {
 		argent -= perte;
-
 	}
 
+	public void faireConnaissance(Humain humain) {
+		direBonjour();
+		humain.repondre(humain);
+		memoriser(humain);
+	}
+
+	private void repondre(Humain humain) {
+		humain.direBonjour();
+		humain.memoriser(this);
+	}
+
+	private void memoriser(Humain humain) {
+
+		if (memoire.size() >= NB_CONNAISSANCES_MAX) {
+
+			memoire.remove(0);
+		}
+		memoire.add(humain);
+	}
+
+	public void afficherMemoire() {
+		String connaissances = "";
+		for (Humain humain : memoire) {
+			connaissances = connaissances + humain.getNom() + ", ";
+			
+		}
+		parler("Je connais beaucoup de monde dont : " + connaissances);
+	}
 }
